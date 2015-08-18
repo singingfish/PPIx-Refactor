c36f47fbcc0060d650083cf2a792db3b3f688abd
Kieren Diment <diment@gmail.com>
2015-08-18 17:44:45 +1000

    docs and fixes

--- lib/PPIx/Refactor.pm
+++ lib/PPIx/Refactor.pm
@@ -24,16 +24,45 @@ our $VERSION = '0.01';
 =head1 SYNOPSIS
 
     use PPIx::Refactor;
-    my $p = PPIx::Refactor->new(file => '...', search => 'RoleName|\&found',
-                                rewrite => 'RoleName|\&found');
+    my $p = PPIx::Refactor->new(file => '/path/to/perl/code/file.pl',
+                                ppi_find => \&found,
+                                [ writer => \&found ]);
+    my $finds = $p->finds; # for examining them interactively
+    $p->rewrite; # rewrites the file in place.  You are using version control yes?
 
-    ...
+=head1 SUMMARY
 
-=head1 ATTRIBUTES
+This is a really simple module to make rewriting perl code via L<PPI>
+debugger friendly and easy.  See the test in
+L<t/refactor.t|https://github.com/singingfish/PPIx-Refactor/blob/master/t/refactor.t>
+of this distribution for a working example.  Pretty much all the real work
+happens in the coderef you set up in C<$p->ppi_find> and C<$p->writer>.
 
+NOTE L<PPI::Cache> is used to store a cached representation of the source
+parse in the system temp directory.
+
+=head2 RATIONALE
+
+Rewriting code via ppi is a fiddly pain.  L<PPIx::Refactor> provides a
+minimal interface so you can concentrate on the fiddlyness and minimise the
+pain.
+
+=head2 TODO
+
+Would be nice to specify a rewriter via roles, and it would be nice to have
+$self in C<$p->ppi_find>.  On the other hand rewrite/refactoring code like
+this can either be simple throwaways, or really really complicated.  This
+code is so far optimised for the throwaway case.
 
 =cut
 
+=head2 ATTRIBUTES
+
+=head3 file
+
+required string that coerces into a Path::Tiny
+
+=cut
 
 has file => (
     is => 'ro',
@@ -41,6 +70,12 @@ has file => (
         path($_[0]);
     }
 );
+
+=head3 doc
+
+lazily built PPI::Document
+
+=cut
 
 has doc => (
     is => 'ro',
@@ -51,16 +86,35 @@ has doc => (
     },
 );
 
+=head3 ppi_find 
+
+required coderef with which to find the elements of interest
+
+=cut
+
 has ppi_find => (
     is => 'ro',
     # isa CodeRef
     required => 1,
 );
 
+
+=head3 writer
+
+optional coderef with which to rewrite the code
+
+=cut
+
 has writer => (
     is => 'ro',
     default => sub {},
 );
+
+=head3 finds
+
+lazy built arrayref of all the elements of interest found
+
+=cut
 
 has finds => (
     is => 'ro',
@@ -73,11 +127,25 @@ has finds => (
     }
 );
 
+=head1 METHODS
+
+=head2 $self->rewrite
+
+Worker sub that rewrites the code
+
+=cut
+
 sub rewrite {
     my ($self) = @_;
     $self->writer->($self->finds);
     $self->doc->save($self->file);
 }
+
+=head2 $self->dump($elem, $whitespace);
+
+For debugging.  Prints a dump of the passed in element.  If whitespace is true it will include whitespace in the dump.  Defaults to false
+
+=cut
 
 sub dump {
     my ($self, $doc, $whitespace) = @_;
@@ -85,9 +153,6 @@ sub dump {
     my $dump = PPI::Dumper->new($doc, whitespace => $whitespace);
     $dump->print;
 }
-
-
-=head1 METHODS
 
 
 =head1 AUTHOR
